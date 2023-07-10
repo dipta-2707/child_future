0f4f9da7289b0987d5f86a5b6e695347c0b983c4
Dipta Das <58099256+dipta-2707@users.noreply.github.com>
2023-07-10 07:43:23 +0600

    Update victim_view.dart

--- lib/views/victim_view.dart
+++ lib/views/victim_view.dart
@@ -1,4 +1,9 @@
+
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../Api.dart';
 
@@ -19,8 +24,11 @@ class VictimView extends StatelessWidget {
                 stream: AppApi.getVictims(),
                 builder:(context, snapshot){
                   if(snapshot.hasData){
+
                     return ListView.separated(
-                        itemBuilder: (context, index) => _victimTile(name: snapshot.data!.docs[index].data()['name'], image: snapshot.data!.docs[index].data()['image']),
+                        itemBuilder: (context, index) => _victimTile(name: snapshot.data!.docs[index].data()['name'], image: snapshot.data!.docs[index].data()['image'],
+                          location:  snapshot.data!.docs[index].data()['address']
+                        ),
                         separatorBuilder: (context, index) => SizedBox(height: 8.0,),
                         itemCount: snapshot.data!.size);
                   }
@@ -36,7 +44,7 @@ class VictimView extends StatelessWidget {
       ),
     );
   }
-  Widget _victimTile({required String name,required String image}){
+  Widget _victimTile({required String name,required String image,required String location,}){
     return DecoratedBox(
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10.0),
@@ -45,10 +53,26 @@ class VictimView extends StatelessWidget {
           )
       ),
       child: ListTile(
+        onTap: (){
+          Get.defaultDialog(
+            title: name,
+            content: Column(
+              children: [
+                Image.network(image,fit: BoxFit.cover,
 
+                  height: Get.height*0.3,
+                  width: Get.width,
+                ),
+                SizedBox(height: 8.0,),
+                Text('Address: $location')
+              ],
+            )
+          );
+        },
         leading: Image.network(image,fit: BoxFit.fill,),
         title: Text(name),
         trailing: Icon(Icons.arrow_circle_right),
+
       ),
     );
   }
